812ddbbb9d28e03e2dcc579c0c9a417699a5f7da
Dan Brickley <danbri@danbri.org>
2015-01-29 14:53:54 +0000

    Cleaned auth info from output.

--- scripts/scanissues.py
+++ scripts/scanissues.py
@@ -12,35 +12,38 @@
 import requests # http://www.python-requests.org/en/latest/
 import json
 import re
+import os
 
 myre = re.compile(r"^\s*http://schema.org/(\w+)", re.MULTILINE)
 
 def getPagedAPI(u):
     r = requests.get(u) 
-
     if (r.ok):
+        if (len(r.json())==0):
+            return("000") 
         for i in r.json():
-          #print i["title"]
-          if "body" in i:
-              body = i["body"]
-              hits = myre.findall(body)
-              for h in hits:
-                  print i["url"]
-                  if "title" in i:
-                      print i["title"]
-                  print "http://schema.org/"+h
-                  print "\n"
-          print
+            if "body" in i:
+                body = i["body"]
+                hits = myre.findall(body)
+                for h in hits:
+                    print "http://schema.org/"+h
+                    print i["url"]
+                    if "title" in i:
+                        print i["title"]
+                        print "\n"
+            print
+        return None   
     else:
             print "# Issue API error."
-            return("404") # ugh:)
+            return("500") 
 
-# TODO: figure out how (headers?) to know how many pages to fetch.
+# Auth - to avoid rate limits, create an OAuth application and put details in GH_AUTH env var.
+# See https://developer.github.com/v3/#rate-limiting
 
+gh_auth = os.environ['GH_AUTH']
 for i in range(10):
-  u = "https://api.github.com/repos/schemaorg/schemaorg/issues?milestone=*;page=%i" % i 
-  print "# Fetching: %s " % u
-  getPagedAPI(u)
-#  if getPagedAPI(u)=="404":
-#    break
-  
+  u = "https://api.github.com/repos/schemaorg/schemaorg/issues?milestone=*;page=%i;%s" % ( i, gh_auth ) 
+  print "# Fetching page %i " % i
+  x = getPagedAPI(u) # bogus return codes
+  if x != None:
+      break
