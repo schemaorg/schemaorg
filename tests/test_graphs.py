824870d0d13d67722c378f4a7095d5e289364ec0
Dan Brickley <danbri@danbri.org>
2015-05-19 17:46:27 +0100

    Moved run_tests.py utility into scripts/ folder, and adjusted tests accordingly.

--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -2,6 +2,8 @@ import unittest
 import os
 import logging # https://docs.python.org/2/library/logging.html#logging-levels
 import glob
+import sys
+sys.path.append( os.getcwd() ) 
 
 from api import *
 from parsers import *
