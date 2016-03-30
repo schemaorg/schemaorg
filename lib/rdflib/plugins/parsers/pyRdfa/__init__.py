c0fb48b29999d6a03fbb86620fbe9edb925c4938
RichardWallis <wallisr@google.com>
2016-03-30 21:41:33 +0100

    Commented out info output of rdfa parser option status

--- lib/rdflib/plugins/parsers/pyRdfa/__init__.py
+++ lib/rdflib/plugins/parsers/pyRdfa/__init__.py
@@ -493,7 +493,7 @@ class pyRdfa :
 		state = ExecutionContext(topElement, default_graph, base=self.required_base if self.required_base != None else "", options=self.options, rdfa_version=self.rdfa_version)
 
 		# Perform the built-in and external transformations on the HTML tree. 
-		logger.info(self.options)
+		#logger.info(self.options)
 		for trans in self.options.transformers + builtInTransformers :
 			trans(topElement, self.options, state)
 		
