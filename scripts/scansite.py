02f4ca6739682095e43bbbe0f7e2b02c7328e62f
Dan Brickley <danbri@google.com>
2016-08-09 10:16:47 +0100

    First working version.

--- scripts/scansite.py
+++ scripts/scansite.py
@@ -9,12 +9,11 @@ from pprint import pprint
 # https://github.com/schemaorg/schemaorg/issues/1285
 # (We could also generate a sitemap.xml file maybe?)
 
-# STATUS: Not working yet. Didn't find /Person from core.
-
 # params		 TODO: pass this in
 v = '3.1' 
-verbose = True
+verbose = False
 
+#verbose = True
 
 fn = 'data/releases/%s/all-layers.jsonld' % v # Per-release JSON-LD dumps.
 
@@ -25,19 +24,33 @@ with open(fn) as data_file:
 if verbose:
   pprint(data) 
 
+# Core is      "@id": "http://schema.org/#3.1"
+
+# TODO: Not robust. Fix w/ regex to handle versions, cases etc.
+#
+def subdomain(gurl):
+  tid = gurl.replace('http://','')
+  tid = tid.replace('#' + str(v),'') # this version
+  tid = tid.replace('.schema.org/','') # ext
+  tid = tid.replace('schema.org/','') # core
+  return( tid )
+
 for g in data["@graph"]:
-  pprint("GRAPH: %s " % g["@id"])
+  pprint("GRAPH: %s" % g["@id"])
   for t in g["@graph"]:
     url = t["@id"]
 
-    if 'schema.org' not in url:
-      break # cases like file:// and wikipedia links show up.
+    if 'schema.org' not in str(url):
+      continue # cases like file:// and wikipedia links show up.
 
-    print url
+    if g['@id'] != "http://schema.org/#%s" % v: # extension
+      exturl = url.replace('schema.org', subdomain(g['@id']) + '.schema.org')
+    else:
+      exturl = '' # core 
 
-    # Note that for extensions we may want the subdomain     
-    # or the bare term, or both, depending on application needs.
+    print "%s,\t%s" % (url, exturl)
+
+    # Note that for extensions we may want the subdomain        
+    # or the bare term, or both, depending on application needs.
     # i.e. http://schema.org/Thesis (the actual type)
     # or http://bib.schema.org/Thesis (the substantive documentation)
-
-# doesn't print Person yet
