c23b4d600e2ee62623ce0e77d6c889493872a154
Dan Brickley <danbri@google.com>
2016-10-27 17:53:03 +0100

    Updated to use different yaml file. Also not posting to sdo-callisto etc.

--- scripts/updateAppEngine.sh
+++ scripts/updateAppEngine.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # first the general development site
-appcfg.py update .   -A webschemas-g
+#appcfg.py update .   -A webschemas-g
+appcfg.py update webschemas.yaml   -A webschemas-g 
 
 # then the release-specific one
 ### not so needed: 
 ### 
-##### appcfg.py update .   -A sdo-deimos
 
-appcfg.py update .   -A sdo-callisto
+# appcfg.py update .   -A sdo-callisto
