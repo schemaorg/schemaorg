a2b4b732c15c3cfefb345354bca8fc6de47d4820
RichardWallis <richard.wallis@dataliberate.com>
2017-03-19 23:16:09 +0000

    Enable NDB Shared memory namespace partioning using engine Version ID

--- appengine_config.py
+++ appengine_config.py
@@ -4,3 +4,12 @@ import vendor
 # third-party libraries, and override built-ins with newer
 # versions.
 vendor.add('lib')
+
+import os
+# Called only if the current namespace is not set.
+def namespace_manager_default_namespace_for_request():
+    # The returned string will be used as the Google Apps domain.
+    applicationVersion="Default"
+    if "CURRENT_VERSION_ID" in os.environ:
+        applicationVersion = os.environ["CURRENT_VERSION_ID"].split('.')[0]
+    return applicationVersion
