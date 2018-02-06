abda136866a48592b8581e062554511c3db0fa3e
Dan Brickley <danbri@danbri.org>
2018-02-06 18:51:04 +0000

    New AppEngine / Cloud tooling

--- scripts/updateAppEngine.sh
+++ scripts/updateAppEngine.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+
+# new:
+
+gcloud app deploy webschemas.yaml --project webschemas-g 
+
+# old:
+
 # first the general development site
 #appcfg.py update .   -A webschemas-g
-appcfg.py update webschemas.yaml   -A webschemas-g 
+#appcfg.py update webschemas.yaml   -A webschemas-g 
 
 # then the release-specific one
 ### not so needed: 
