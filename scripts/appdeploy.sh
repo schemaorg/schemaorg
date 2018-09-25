f03097d1eac6a0d555b09e83ec72cbd8d15de24c
Dataliberate <rjw@dataliberate.com>
2018-09-25 11:22:48 +0100

    added a little more robustness to deploy script

--- scripts/appdeploy.sh
+++ scripts/appdeploy.sh
@@ -3,9 +3,15 @@ PWD=`pwd`
 PROG="`basename $0`"
 if [ `basename $PWD` != "schemaorg" ]
 then
-	echo "Not in the schemaorg directory! Aborting"
+	echo "$PROG: Not in the schemaorg directory! Aborting"
 	exit 1
 fi
+
+if [ ! -d ./admin ]
+then
+    mkdir ./admin
+fi
+
 date -u > ./admin/deploy_timestamp.txt
 
 echo gcloud app deploy $@
