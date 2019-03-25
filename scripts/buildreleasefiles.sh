732a5ec77e0356ec89bdc7f9804f1e4403ca09b7
Dataliberate <rjw@dataliberate.com>
2019-03-25 11:08:27 +0000

    Updated buildreleasefiles.sh to take parameters to select output type, auto run etc. Updated .travis.yml to call buildreleasefiles.sh building only required outputs.

--- scripts/buildreleasefiles.sh
+++ scripts/buildreleasefiles.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+set -e
+set -u
+
 EXTENSIONS="attic auto bib health-lifesci pending meta"
 PWD=`pwd`
 PROG="`basename $0`"
@@ -7,39 +10,78 @@ then
 	echo "Not in the schemaorg directory! Aborting"
 	exit 1
 fi
+
+function usage {
+    echo "usage: $(basename $0) [-y] [-e] [-c] [-o] [-limit somevalue] VERSION"
+    echo "    -y   Assume yes to continue"
+    echo "    -e   No extentstions (only produce core and all-layers)"
+    echo "    -c   No context file"
+    echo "    -o   No owl file"
+    echo "    -l   \"Output types\" (json-ld|turtle|nt|nquads|rdf|csv)" 
+}
+
 if [ "$#" -lt 1 ]
 then
-  echo "Usage: $0 VERSION [yes]" 
+usage
   exit 1
 fi
+
+LIMIT=""
+AUTORUN=0
+CONTEXT=1
+OWL=1
+EXTS=1
+while getopts 'yecol:' OPTION; do
+  case "$OPTION" in
+    y)
+        AUTORUN=1
+        echo "Run unchallenged"
+    ;;
+
+    c)
+        CONTEXT=0
+    ;;
+
+    o)
+        OWL=0
+    ;;
+    
+    e)
+        EXTS=0
+    ;;
+
+
+    l)
+        LIMIT="$OPTARG"
+        echo "The limit value provided is $OPTARG"
+    ;;
+    ?)
+        usage 
+        exit 1
+    ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+VER="$*"
+
+
 VER=$1
 DIR="./data/releases/$1"
 
-pre=$2
-echo "PRE: $pre"
-response=""
 
-if [ "$#" -eq 2 ] 
-then
-    if [ "$pre" == 'yes' ]
-    then
-        response="Y"
-    fi
-fi
-
-if [ -z "$response" ]
+if [ $AUTORUN -eq 0 ]
 then
     echo "$PROG:\n\tAbout to build release files for version $VER  \n\tIncluding extensions: $EXTENSIONS"
     read -r -p  "Continue? y/n: " response
+    case $response in
+    	[yY])
+    		AUTORUN=1
+    	;;
+    	*)
+    		echo "Aborting!"
+    	exit 0
+    esac
 fi
-case $response in
-	[yY])
-		echo
-	;;
-	*)
-		echo "Aborting!"
-	exit 0
-esac
 
 if [ ! -d  $DIR ]
 then
@@ -72,34 +114,52 @@ function dump {
 		ex2="extensions"
 	fi
 	file=$3
+    LIMIT=$4
 	forms=""
-    for form in json-ld turtle nt nquads rdf csv
-	do
-        forms="$forms -f $form"
+    if [ -z "$LIMIT" ]
+    then
+        for form in json-ld turtle nt nquads rdf csv
+    	do
+            forms="$forms -f $form"
         
-    done
+        done
+    else
+        forms=$LIMIT
+    fi
+    
 	./scripts/exportgraphs.py -i "$in" -e "$ex1" -e "$ex2" -g "#$VER" $forms -o $DIR/$file 2>&1 > /dev/null
 }
 
 echo
 echo "Creating core: "
-dump core extensions schema
+dump core extensions schema "$LIMIT"
+
 echo
 echo "Creating all-layers: "
-dump "" "" all-layers
-for e in $EXTENSIONS
-do
-	echo
-	echo "Creating $e"
-	dump "$e" "ALL" "ext-$e"
-done
+dump "" "" all-layers "$LIMIT"
 
-echo "Creating archive context file"
+if [ $EXTS -eq 1 ]
+then
+    for e in $EXTENSIONS
+    do
+    	echo
+    	echo "Creating $e"
+    	dump "$e" "ALL" "ext-$e" "$LIMIT"
+    done
+fi
 
-./scripts/buildarchivecontext.py -o schemaorgcontext.jsonld -d $DIR
 
-echo "creating owl file"
-./scripts/buildowlfile.py
+if [ $CONTEXT -eq 1 ]
+then
+    echo "Creating archive context file"
+    ./scripts/buildarchivecontext.py -o schemaorgcontext.jsonld -d $DIR
+fi
+
+if [ $OWL -eq 1 ]
+then
+    echo "creating owl file"
+    ./scripts/buildowlfile.py
+fi
 echo done
 
 
