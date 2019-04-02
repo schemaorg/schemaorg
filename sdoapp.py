a559549e44842f6011c90160fffb8624f0e04343
Dataliberate <rjw@dataliberate.com>
2019-04-02 15:35:37 +0100

    Fixed redering of docs/tree.jsonld

--- sdoapp.py
+++ sdoapp.py
@@ -1772,9 +1772,8 @@ class ShowUnit (webapp2.RequestHandler):
             log.debug("Serving recycled JSONLDThingTree.")
             return True
         else:
-            uThing = Unit.GetUnit("Thing")
             mainroot = TypeHierarchyTree()
-            mainroot.traverseForJSONLD(Unit.GetUnit("Thing"), layers=layerlist)
+            mainroot.traverseForJSONLD(VTerm.getTerm("Thing"), layers=layerlist)
             thing_tree = mainroot.toJSON()
             self.response.out.write( thing_tree )
             log.debug("Serving fresh JSONLDThingTree.")
