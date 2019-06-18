ac9a679770b838a878d65be90f34b50c352efa3d
Richard Wallis <richard.wallis@dataliberate.com>
2019-06-18 16:26:08 +0100

    Removed unnecessary newlines from comments in CSV files. (#2269)
    Correted missing 'label' field value in CSV property outputs
    Fixes (#2262)

--- sdordf2csv.py
+++ sdordf2csv.py
@@ -95,6 +95,10 @@ class sdordf2csv():
             graph = self.queryGraph
         if term == None or graph == None:
             return
+
+        if not isinstance(term, URIRef):
+            term = URIRef(term)
+
         row = [str(term)]
         row.append(self.graphValueToCSV(subject=term,predicate=RDFS.label,graph=graph))
         row.append(self.getCSVComment(term,graph=self.fullGraph))
@@ -290,4 +294,4 @@ class sdordf2csv():
             Markdown.setPre(api.SdoConfig.vocabUri())
             ret = Markdown.parse(ret)
             Markdown.setPre()
-        return ret
+        return ret.replace('\n','')
