3b481a45bf42e9d368fe5205361a079bb5456954
Dataliberate <rjw@dataliberate.com>
2020-03-17 09:25:33 +0000

    Ennhanced reporting

--- scripts/runpythonpostprepare.sh
+++ scripts/runpythonpostprepare.sh
@@ -5,13 +5,12 @@ set -u
 PWD="`pwd`"
 PROG="`basename $0`"
 
-
 if [  ${SECUREHTTP+"false"} ] #If $SECUREHTTP is set, use the handlers-secure.yaml
 then
-    echo "$PROG: Copying handlers-secure.yaml to handlers.yaml - https 301 redirect"
+    echo "$PROG: Copying handlers-secure.yaml to handlers.yaml - implements https 301 redirect"
     cp ./handlers-secure.yaml sdopythonapp/site/handlers.yaml
 else
-    echo "$PROG: Copying handlers.yaml to handlers.yaml - http or https"
+    echo "$PROG: Copying handlers.yaml to handlers.yaml - http or https (No 301 redirect)"
     cp ./handlers.yaml sdopythonapp/site/handlers.yaml
 fi
 echo
