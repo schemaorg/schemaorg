aafcbfa4128074eb67264b21b8cd55b27833504b
Pascal Fleury <fleury@google.com>
2024-10-03 14:31:28 +0200

    Made an object for Schema.org graphs, to share logic across commands.

--- software/util/reorg.py
+++ software/util/reorg.py
@@ -6,64 +6,80 @@ semantic axis.
 """
 
 from collections.abc import Sequence
-from rdflib import Graph, Namespace
+import rdflib
+import argparse
+import itertools
 import logging
 import sys
-import argparse
 
-def SchemaOrgGraph():
-  """Creates a graph with usual namespaces set as we want in schemaorg."""
-  g = Graph()
-  g.bind("dc", Namespace("http://purl.org/dc/terms/"), replace=True)
-  return g
+SCHEMAORG = rdflib.Namespace("https://schema.org/")
 
-def GraphsAreIdentical(g1, g2):
-  only_in_g1 = g1 - g2
-  only_in_g2 = g2 - g1
-  # TODO: print sampled diffs if any.
-  return len(only_in_g1) == 0 and len(only_in_g2) == 0
+class SchemaOrgGraph(object):
+  def __init__(self, filename: str = None, format: str = "turtle"):
+    self.g = rdflib.Graph()
+    # Binding it here, as by default it would bind the /elements/1.1/ instead
+    # of the dc terms. this way, the elements get assigned 'dc1' or such
+    # as a prefix, and we do not use that.
+    self.g.bind("dc", rdflib.Namespace("http://purl.org/dc/terms/"), replace=True)
+    if filename:
+      self.g.parse(filename, format=format)
 
-def GraphIsIncludedIn(g_subset, g_universe):
-  only_in_subset = g_subset - g_universe
-  # TODO: print sampled diffs if any.
-  return len(only_in_subset) == 0
+  def __getattr__(self, *args, **kwargs):
+    return getattr(self.g, *args, **kwargs);
+
+  def IdenticalTo(self, other: "SchemaOrgGraph"):
+    only_in_other = other.g - self.g
+    only_in_self = self.g - other.g
+    if len(only_in_other) or len(only_in_self):
+      raise ValueError(f"Graphs differ: {only_in_other + only_in_self}")
+    return True
+
+  def FullyContains(self, graph: "SchemaOrgGraph"):
+    only_in_subset = graph.g - self.g
+    if len(only_in_subset):
+      raise ValueError(f"Graph does not contain it all: {only_in_subset}")
+    return True
+
+  def ListSubjects(self, subject_type: rdflib.term.URIRef):
+    return set([s for s, p, o in self.g.triples((None, rdflib.RDF.type, subject_type))])
+
+  def Types(self):
+    return self.ListSubjects(rdflib.RDFS.Class)
+
+  def Properties(self):
+    return self.ListSubjects(rdflib.RDF.Property)
+
 
 def Lint(args):
   """Reformats the file(s) properly."""
-  def LintOne(filename, output_filename):
+  def LintOne(filename, output_filename, format):
     logging.info(" - reading file ...")
-    g = SchemaOrgGraph()
-    g.parse(filename, format="turtle")
+    g = SchemaOrgGraph(filename, format=format)
     logging.info(f" - writing back {output_filename if output_filename != filename else ''} ...")
-    g.serialize(output_filename, format="turtle")
-
-    v = SchemaOrgGraph()
-    v.parse(output_filename, format="turtle")
-    if not GraphsAreIdentical(v, g):
+    g.serialize(output_filename, format=format)
+    if not g.IdenticalTo(SchemaOrgGraph(output_filename, format=format)):
       logging.fatal(f"Linting file {filename} lost some information.")
-    logging.info(" - validated.")
 
   if args.output is not None and len(args.files) > 1:
     logging.fatal(f"Cannot use --output with multiple files!")
   for index, filename in enumerate(args.files):
     logging.info(f"Handling file {index} of {len(args.files)} ({filename})")
-    LintOne(filename, args.output or filename)
+    LintOne(filename, args.output or filename, format="turtle")
+    logging.info(" - validated.")
 
 
 def MergeFiles(args):
   """Merges a set of files into one."""
-  merged = SchemaOrgGraph()
   logging.info(f"Merging files {len(args.files)} into {args.output}")
+  merged = SchemaOrgGraph()
   for filename in args.files:
     logging.info(f" - reading {filename} ...")
     merged.parse(filename, format="turtle")
   logging.info(f"Writing {args.output} ...")
-  merged.serialize(args.output)
+  merged.serialize(args.output, format="turtle")
 
   for filename in args.files:
-    v = SchemaOrgGraph()
-    v.parse(filename, format="turtle")
-    if not GraphIsIncludedIn(v, merged):
+    if not merged.FullyContains(SchemaOrgGraph(filename, format="turtle")):
       logging.fatal(f"Merging files into {args.output} lost some information.")
 
 
@@ -81,11 +97,11 @@ def main():
                                       help='Reformat files (aka linting)')
   lint_parser.add_argument('-o', '--output',
                            help='Output file (default: overwrites)')
-  lint_parser.add_argument("files", action="extend", nargs="+", type=str);
+  lint_parser.add_argument("files", action="extend", nargs="+", type=str)
 
   merge_parser = subparsers.add_parser('merge', help='Merging several files')
   merge_parser.add_argument('-o', '--output', help='Output file')
-  merge_parser.add_argument("files", action="extend", nargs="+", type=str);
+  merge_parser.add_argument("files", action="extend", nargs="+", type=str)
 
   # Parse and dispatch
   args = parser.parse_args()
